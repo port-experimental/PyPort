72ea2e667a0fea81b8f11ad7337b59180ffbdb83
dan-amzulescu-port <dan.a@getport.io>
2025-05-03 06:46:34 -0500

    0.1.22
    fixes to actions

--- src/pyport/actions/actions_api_svc.py
+++ src/pyport/actions/actions_api_svc.py
@@ -11,7 +11,7 @@ class Actions(BaseResource):
 
         :return: A list of action dictionaries.
         """
-        response = self._client.make_request('GET', "/v1/actions")
+        response = self._client.make_request('GET', "actions")
         return response.json().get("actions", [])
 
     def get_action(self, action_identifier: str) -> Dict:
@@ -21,7 +21,7 @@ class Actions(BaseResource):
         :param action_identifier: The identifier of the action.
         :return: A dictionary representing the action.
         """
-        response = self._client.make_request('GET', f"/v1/actions/{action_identifier}")
+        response = self._client.make_request('GET', f"actions/{action_identifier}")
         return response.json().get("action", {})
 
     def create_action(self, action_data: Dict) -> Dict:
@@ -32,7 +32,7 @@ class Actions(BaseResource):
         :param action_data: A dictionary containing data for the new action.
         :return: A dictionary representing the created action.
         """
-        response = self._client.make_request('POST', f"/v1/actions", json=action_data)
+        response = self._client.make_request('POST', f"actions", json=action_data)
         return response.json()
 
     def update_action(self, blueprint_identifier: str, action_identifier: str, action_data: Dict) -> Dict:
@@ -44,7 +44,7 @@ class Actions(BaseResource):
         :param action_data: A dictionary containing updated data for the action.
         :return: A dictionary representing the updated action.
         """
-        response = self._client.make_request('PUT', f"/v1/actions/{action_identifier}",
+        response = self._client.make_request('PUT', f"actions/{action_identifier}",
                                              json=action_data)
         return response.json()
 
@@ -56,7 +56,7 @@ class Actions(BaseResource):
         :param action_identifier: The identifier of the action to delete.
         :return: True if deletion was successful (e.g., status code 204), else False.
         """
-        response = self._client.make_request('DELETE', f"/v1/actions/{action_identifier}")
+        response = self._client.make_request('DELETE', f"actions/{action_identifier}")
         return response.status_code == 204
 
     def get_action_permissions(self, blueprint_identifier: str, action_identifier: str) -> Dict:
@@ -68,7 +68,7 @@ class Actions(BaseResource):
         :return: A dictionary representing the action's status.
         """
         response = self._client.make_request(
-            'GET', f"/v1/actions/{action_identifier}/permissions"
+            'GET', f"actions/{action_identifier}/permissions"
         )
         return response.json().get("status", {})
 
@@ -81,6 +81,6 @@ class Actions(BaseResource):
         :return: True if cancellation was successful (e.g., status code 200), else False.
         """
         response = self._client.make_request(
-            'POST', f"/v1/actions/{action_identifier}/permissions"
+            'POST', f"actions/{action_identifier}/permissions"
         )
         return response.status_code == 200
